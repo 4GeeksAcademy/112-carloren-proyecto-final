eb391b9b0e84b8827a5888e8d9a4c4d846891a7d
Carlos Lorenzo <carloren.1996@gmail.com>
2025-08-17 19:35:12 +0000

    objetivos inicio

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -11,11 +11,11 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<div className="container col-xxl-8 px-4 py-5 text-bg-primary rounded-5" >
-				<div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+				<div className="row flex-lg-row-reverse align-items-center g-5">
 					<div className="mx-auto col-10 col-sm-8 col-lg-6">
 						<img src="https://profesionales-malaga.com/multiservicios/imagenes/Blog/ventajas-de-contratar-una-empresa.jpg" className="d-block mx-lg-auto img-fluid rounded-4" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
 					</div>
-					<div className="col-lg-6 text-justify">
+					<div className="col-lg-6 text-start">
 						<h1 className="fw-bold lh-1 mb-3">¡Encuentra un profesional en segundos!</h1>
 						<p className="lead"> Aquí podrás encontrar en un par de pasos un profesional que relice el servicio que necesitas. ¡Registrate para contratar servicios o para poder ofrecerlos!</p>
 						<div className="d-grid gap-2 d-md-flex justify-content-md-start  mb-3">
@@ -35,7 +35,7 @@ export const Home = () => {
 			<div className="container px-4 py-5" id="featured-3">
 				<div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
 					<div className="feature col d-flex flex-column">
-						<div className="border rounded-4 text-bg-secondary fs-4 mb-3 py-2 px-3 mx-auto">
+						<div className="border rounded-4 text-bg-primary fs-4 mb-3 py-2 px-3 mx-auto">
 							<i class="bi bi-pin-angle"></i>
 						</div>
 						<h3 className="fs-2 text-body-emphasis">Quiénes somos</h3>
@@ -43,7 +43,7 @@ export const Home = () => {
 					</div>
 
 					<div className="feature col d-flex flex-column">
-						<div className="border rounded-4 text-bg-secondary fs-4 mb-3 py-2 px-3 mx-auto">
+						<div className="border rounded-4 text-bg-primary fs-4 mb-3 py-2 px-3 mx-auto">
 							<i class="bi bi-bullseye"></i>
 						</div>
 						<h3 className="fs-2 text-body-emphasis">Objetivo</h3>
@@ -52,11 +52,11 @@ export const Home = () => {
 					</div>
 
 					<div className="feature col d-flex flex-column">
-						<div className="border rounded-4 text-bg-secondary fs-4 mb-3 py-2 px-3 mx-auto">
+						<div className="border rounded-4 text-bg-primary fs-4 mb-3 py-2 px-3 mx-auto">
 							<i class="bi bi-search"></i>
 						</div>
 						<h3 className="fs-2 text-body-emphasis">Cómo funciona</h3>
-						<p>Como usuario podrás buscar todo tipo de servicios en un par de clics, filtrando por tipo de servicio o cercanía.
+						<p>Como usuario podrás buscar filtrando por tipo de servicio o cercanía.
 							Deberás registrarte para contactar con el profesional.
 							Como profesional podrás crear una cuenta ofreciendo tus servicios para que los usuarios puedan encontrarte y solicitar tu trabajo</p>
 					</div>
